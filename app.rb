ce3eab746c38d16d6e580e0207c3c04cfcb8656a
Jennifer Miller <jmillerarcher@me.com>
2020-02-01 20:10:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
+   get '/' do
+     
+		erb :index	
+		
+end		
+	
+	
+	
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
 		
 	end
-end
+	
+
